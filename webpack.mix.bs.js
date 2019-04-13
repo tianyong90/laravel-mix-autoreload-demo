5cfb9e275ad462510ec8d9b3f161dc0732dca512
tianyong90 <412039588@qq.com>
2019-04-13 16:38:21 +0800

    multiple mix file

---
+++ webpack.mix.bs.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+   proxy: 'laravel-mix-autoreload-demo.test/',
+   startPath: '',
+   open: false,
+   reloadOnRestart: true,
+   watchOptions: {
+      usePolling: true,
+   },
+})
